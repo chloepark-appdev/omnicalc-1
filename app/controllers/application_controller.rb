5fa7ce24bb63ba97c1129a191336d985da54c3d9
chloepark195 <spark220@chicagobooth.edu>
2022-01-31 18:00:38 +0000

    Formatting finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,58 @@
 class ApplicationController < ActionController::Base
   def blank_square_form
-
-    render({ :template =>"calculation_templates/square_from.html.erb"})
+    render({ :template => "calculation_templates/square_from.html.erb" })
   end
 
   def caculate_square
-      # params = {"elephant"=>"42"}
+    # params = {"elephant"=>"42"}
 
-      @num = params.fetch("elephant").to_f
-      @square_of_num = @num ** 2
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num ** 2
 
-    render({ :template =>"calculation_templates/square_results.html.erb"})
-
+    render({ :template => "calculation_templates/square_results.html.erb" })
   end
+
+  def blank_squareroot_form
+    # params = {"elephant"=>"42"}
+
+    # binding.pry
+
+    render({ :template => "calculation_templates/square_root_from.html.erb" })
+  end
+
+  def caculate_squareroot
+    @num = params.fetch("rabbit").to_f
+    @square_of_num = @num ** (0.5)
+
+    render({ :template => "calculation_templates/square_root_results.html.erb" })
+  end
+
+
+  def blank_random_form
+   
+    render({ :template => "calculation_templates/random_form.html.erb" })
+  end
+
+  def caculate_random
+    @num = params.fetch("tree").to_f
+    @num = params.fetch("tree").to_f
+    @rand_number= rand().to_f
+
+    render({ :template => "calculation_templates/random_results.html.erb" })
+  end
+
+  def blank_payment_form
+   
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def caculate_payment
+    @num = params.fetch("april").to_f
+    @num = params.fetch("years").to_f
+    @num = params.fetch("principal").to_f
+     
+
+    render({ :template => "calculation_templates/payment_results.html.erb" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,18 @@ Rails.application.routes.draw do
 
     get("/square/results", { :controller=> "application", :action =>"caculate_square" })
 
+    get("/square_root/new", { :controller=> "application", :action =>"blank_squareroot_form" })
+
+    get("/square_root/results", { :controller=> "application", :action =>"caculate_squareroot" })
+
+    get("/random/new", { :controller=> "application", :action =>"blank_random_form" })
+
+    get("/random/results", { :controller=> "application", :action =>"caculate_random" })
+
+    get("/payment/new", { :controller=> "application", :action =>"blank_payment_form" })
+
+    get("/payment/results", { :controller=> "application", :action =>"caculate_payment" })
+
+
 end
+
