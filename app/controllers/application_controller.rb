1acc892dfcd2de698914f197e31834c5b08f3ac3
chloepark195 <spark220@chicagobooth.edu>
2022-02-04 17:01:37 +0000

    Git commit test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,8 +34,8 @@ class ApplicationController < ActionController::Base
   end
 
   def caculate_random
-    @num = params.fetch("tree").to_f
-    @num = params.fetch("tree").to_f
+    @num = params.fetch("user_min").to_f
+    @num_1 = params.fetch("user_max").to_f
     @rand_number= rand().to_f
 
     render({ :template => "calculation_templates/random_results.html.erb" })
@@ -47,10 +47,11 @@ class ApplicationController < ActionController::Base
   end
 
   def caculate_payment
-    @num = params.fetch("april").to_f
-    @num = params.fetch("years").to_f
-    @num = params.fetch("principal").to_f
-     
+    @apr = params.fetch("a_input").to_f
+    @years = params.fetch("y_input").to_i
+    @principal = params.fetch("p_input").to_i
+    
+    @result = (@apr/12/100)*@principal / ( 1 - (1 + (@apr/12/100)) ** ( -@years * 12 ))
 
     render({ :template => "calculation_templates/payment_results.html.erb" })
   end
